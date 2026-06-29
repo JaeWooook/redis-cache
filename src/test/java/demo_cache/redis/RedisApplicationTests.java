5c86808a7adca4cd9fab2e02f80e73c35dfb07e3
jeong.jaewook <jeong.jaewook@joins.com>
2026-06-29 10:24:49 +0900

    Initial commit: Spring Boot Redis demo project

---
+++ src/test/java/demo_cache/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package demo_cache.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
